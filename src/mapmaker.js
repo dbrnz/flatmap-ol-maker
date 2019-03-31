d782cda6fd1f680aab09c2b0712d499aab8084db
David Brooks <dave@bcs.co.nz>
2019-03-31 21:33:45 +1300

    Use `puppeteer` to rasterise SVG before tiling, and use `jimp` for image operations.

--- src/mapmaker.js
+++ src/mapmaker.js
@@ -26,6 +26,9 @@ limitations under the License.
 const fs = require('fs');
 const path = require('path');
 
+const Jimp = require('jimp');
+const puppeteer = require('puppeteer');
+
 //==============================================================================
 
 const TILE_PIXELS = [256, 256];
@@ -48,10 +51,104 @@ class MapMaker
 		this._outputDirectory = outputDirectory;
 	}
 
-	tile(layer)
-	{
-		console.log(layer.id);
-	}
+    async readSvg(filePath)
+    {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                if (err) reject(err)
+                else resolve(data);
+            })
+        });
+    }
+
+    async writeTiles(tileData, filePath)
+    {
+        const image = await Jimp.create(Buffer.from(tileData.substr('data:image/png;base64,'.length), 'base64'));
+        image.write(filePath);
+    }
+
+    async createTiles(options)
+    {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        async function tileZoomLevel(svg, options, scale)
+        {
+          return new Promise((resolve, reject) =>
+          {
+            const img = new Image();
+
+            const onLoad = () =>
+            {
+                let imageWidth = scale*img.naturalWidth;
+                let imageHeight = scale*img.naturalHeight;
+                canvas.width = imageWidth;
+                canvas.height = imageHeight;
+
+                // Set background color
+                ctx.fillStyle = '#00000000';
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+                //const clip = { x: 0, y:0, width: 640, height: 360 };
+                //ctx.drawImage(img, clip.x, clip.y, clip.width, clip.height, 0, 0, imageWidth, imageHeight);
+                ctx.drawImage(img, 0, 0, imageWidth, imageHeight);
+
+                const dataURI = canvas.toDataURL('image/png');
+                document.body.removeChild(img);
+
+                this.writeTiles(dataURI, options).then(() => resolve());
+            }
+
+            const onError = (e) =>
+            {
+              document.body.removeChild(img);
+              reject(`ERROR: ${e}`);
+            }
+
+            img.addEventListener("load", onLoad);
+            img.addEventListener("error", onError);
+
+            document.body.appendChild(img);
+
+            img.src = `data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(svg)))}`;
+
+          });
+        }
+
+        async function tileLayer(options)
+        {
+            const svg = await this.readSvg(options.svg);
+
+            // For all zoom levels
+
+            const res1 = tileZoomLevel(svg, `s1-${options.png}`, 1);
+            const res2 = tileZoomLevel(svg, `s2-${options.png}`, 2);
+
+            return Promise.all([res1, res2]);
+        }
+
+        // For all layers
+//        for (const layer of this._map.layers) {
+//            tileMaker.tile(layer);
+//        }
+
+        const layer1 = tileLayer(options);
+        return Promise.all([layer1]);
+    }
+
+    async makeTiles(options)
+    {
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-dev-shm-usage']});
+        const page = await browser.newPage();
+
+        await page.exposeFunction('readSvg', this.readSvg);
+        await page.exposeFunction('writeTiles', this.writeTiles);
+
+        await page.evaluate(this.createTiles, options);
+
+        await browser.close();
+    }
+
 }
 
 //==============================================================================
@@ -87,9 +184,8 @@ function main()
 	const mapMaker = new MapMaker(map, outputDirectory);
 
 	try {
-		for (const layer of map.layers) {
-			mapMaker.tile(layer);
-		}
+        const options = {svg: 'f.svg', png: 'f.png'};
+        mapMaker.makeTiles(options);
 	} catch (e) {
 		console.error(e.message);
 	}
