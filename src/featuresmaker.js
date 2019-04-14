b2eb6a117858db73cdb76f251c57669663ce4aad
David Brooks <dave@bcs.co.nz>
2019-04-14 16:14:59 +1200

    Convert SVG paths into GeoJSON features when making a map.

---
+++ src/featuresmaker.js
@@ -0,0 +1,260 @@
+/******************************************************************************
+
+Flatmap viewer and annotation tool
+
+Copyright (c) 2019  David Brooks
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+******************************************************************************/
+
+'use strict';
+
+//==============================================================================
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const et = require('elementtree');
+
+const simplify = require('simplify');
+
+const {SVG, registerWindow, select} = require('@svgdotjs/svg.js');
+const svgdom = require('svgdom');
+const svgflatten = require('svg-flatten');
+const svgpath = require('svgpath');
+
+//==============================================================================
+
+function setChildParents(node) {
+    for (const child of node.getchildren()) {
+        child.parent = node;
+        setChildParents(child);
+    }
+}
+
+//==============================================================================
+
+function round(x, decimals=2)
+{
+    const m = Math.pow(10, decimals);
+    return Math.floor(m*x + 0.5)/m;
+}
+
+//==============================================================================
+
+function differentPaths(path1, path2, tolerance=1)
+{
+    if (path1.length === path2.length) {
+        for (let i = 0; i < path1.length; i++) {
+            if (Math.abs(path1[i][0] - path2[i][0]) > tolerance
+             || Math.abs(path1[i][1] - path2[i][1]) > tolerance) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return true;
+}
+
+//==============================================================================
+
+class LayerFeatures
+{
+    constructor(layer, mapSize, SVGDrawElement)
+    {
+        const svg = fs.readFileSync(layer.source, 'utf8');
+        const flatSvg = svgflatten(svg).pathify().value();
+
+        this._xml = et.parse(flatSvg).getroot();
+        setChildParents(this._xml);
+
+        this._svgWidth = parseFloat(this._xml.attrib.width);
+        this._svgHeight = parseFloat(this._xml.attrib.height);;
+        this._svgBBox = [
+            [0, 0],
+            [this._svgWidth, 0],
+            [this._svgWidth, this._svgHeight],
+            [0, this._svgHeight],
+            [0, 0]
+        ];
+
+        if (layer.sourceExtent) {
+            this._svgExtent = layer.sourceExtent;
+        } else {
+            this._svgExtent = [0, 0, this._svgWidth, this._svgHeight];
+        }
+
+        if (layer.resolution) {
+            this._mapSize = [layer.resolution*this._svgExtent[2],
+                             layer.resolution*this._svgExtent[3]];
+        } else {
+            this._mapSize = mapSize;
+        }
+
+        this._tolerance = 0.3;   // A function of svg size??
+
+        this._SVGDrawElement = SVGDrawElement
+    }
+
+    pointToMap_(pt)
+    //=============
+    {
+        return [round(this._mapSize[0]*(pt[0] - this._svgExtent[0])/this._svgExtent[2]),
+                round(this._mapSize[1]*(this._svgHeight - this._svgExtent[1] - pt[1])/this._svgExtent[3])];
+    }
+
+    simplifiedPathPoints_(pathNode)
+    //=============================
+    {
+        let pathDescription = svgpath(pathNode.attrib.d);
+        let node = pathNode;
+        while (node && node.tag !== 'svg') {
+            if (node.attrib.transform) {
+                pathDescription = pathDescription.transform(node.attrib.transform);
+            }
+            node = node.parent;
+        }
+        const path = this._SVGDrawElement.path(pathDescription.toString());
+        const len = path.length();
+        const pts = [];
+        for (let i = 0; i <= 1000; ++i) {
+            pts.push(path.pointAt(i*len/1000));
+        }
+
+        const result = [];
+        for (let pt of simplify(pts, this._tolerance)) {
+            result.push([pt.x, pt.y]);
+        }
+        return {
+            data: result,
+            hash: `${result.length}/${len.toPrecision(5)}`
+        }
+    }
+
+    pathToGeoJsonFeature_(points, id)
+    //===============================
+    {
+        const coords = [];
+        let geometry = 'Polygon';
+        const pt0 = points[0];
+        coords.push(this.pointToMap_(pt0));
+        const lastIndex = points.length - 1;
+        for (let i = 1; i < lastIndex; ++i) {
+            const pt = points[i];
+            coords.push(this.pointToMap_(pt));
+        }
+        const pt = points[lastIndex];
+        if (pt0[0] !== pt[0] || pt0[1] !== pt[1]) {
+            coords.push(this.pointToMap_(pt));
+            geometry = 'LineString';
+        }
+
+        const feature = {
+            type: "Feature",
+            geometry: {
+                type: geometry,
+                coordinates: null
+            },
+            id: id
+        };
+        if (geometry === 'LineString') {
+            feature.geometry.coordinates = coords;
+        } else {
+            feature.geometry.coordinates = [coords];
+        }
+        return feature;
+    }
+
+    extendGeoJson(geoJson)
+    //====================
+    {
+        const hashMap = new Map();   // hash --> points data
+
+        for (const path of this._xml.findall('.//path')) {
+            if (path.parent.tag !== 'clipPath') {
+                const points = this.simplifiedPathPoints_(path);
+                if (differentPaths(this._svgBBox, points.data)) {
+                    const newPath = hashMap.has(points.hash)
+                                    ? differentPaths(hashMap.get(points.hash), points.data, this._tolerance)
+                                    : true;
+                    if (newPath) {
+                        const feature = this.pathToGeoJsonFeature_(points.data, path.attrib.id);
+                        geoJson.features.push(feature);
+                        hashMap.set(points.hash, points.data);
+                        console.log('Path', path.attrib.id);
+                    }
+                }
+            }
+        }
+    }
+}
+
+//==============================================================================
+
+class FeaturesMaker
+{
+    constructor(map, outputDirectory)
+    {
+        this._map = map;
+        this._outputDirectory = outputDirectory;
+    }
+
+    makeFeatures()
+    //============
+    {
+        const document = svgdom.document;
+        registerWindow(svgdom, svgdom.document);
+        const SVGDrawElement = SVG(document.documentElement);
+
+        const featuresSourceDir = path.resolve(this._map.inputDirectory, 'features');
+        const featuresOutputDir = path.join(this._outputDirectory, 'features');
+
+        for (const layer of this._map.layers) {
+            const featureSourceFile = path.join(featuresSourceDir, `${layer.id}.json`);
+            let geoJson = null;
+            if (fs.existsSync(featureSourceFile)) {
+                const featureData = fs.readFileSync(featureSourceFile);
+                geoJson = JSON.parse(featureData);
+                if (!geoJson.id) {
+                    geoJson.id = layer.id
+                } else if (geoJson.id !== layer.id) {
+                    throw new Error(`Layer ${layer.id} has wrong feature ID (${geoJson.id})`);
+                }
+            } else {
+                geoJson = {
+                    type: "FeatureCollection",
+                    features: [],
+                    id: layer.id
+                };
+            }
+
+            const layerFeatures = new LayerFeatures(layer, this._map.size, SVGDrawElement);
+
+            layerFeatures.extendGeoJson(geoJson);
+
+            if (!fs.existsSync(featuresOutputDir)) {
+                fs.mkdirSync(featuresOutputDir, {mode: 0o755});
+            }
+
+            fs.writeFileSync(path.join(featuresOutputDir, `${layer.id}.json`,
+                             JSON.stringify(geoJson, null, 2));
+        }
+    }
+}
+
+//==============================================================================
+
+module.exports.FeaturesMaker = FeaturesMaker;
+
+//==============================================================================
---
+++ src/tilemaker.js
@@ -0,0 +1,271 @@
+/******************************************************************************
+
+Flatmap viewer and annotation tool
+
+Copyright (c) 2019  David Brooks
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+******************************************************************************/
+
+'use strict';
+
+//==============================================================================
+
+const crypto = require('crypto');
+const fs = require('fs-extra');
+const path = require('path');
+
+const sizeOf = require('image-size');
+const Jimp = require('jimp');
+const puppeteer = require('puppeteer');
+
+//==============================================================================
+
+const cropImage = require('./cropimage');
+
+//==============================================================================
+
+const TILE_PIXEL_SIZE = [256, 256];
+
+//==============================================================================
+
+async function svgToPng(svgBase64, svgExtent, imageSize)
+{
+    const canvas = document.createElement('canvas');
+    canvas.width = imageSize[0];
+    canvas.height = imageSize[1];
+
+    const ctx = canvas.getContext('2d');
+
+    // Set transparent background
+    ctx.fillStyle = '#00000000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const img = new Image();
+    document.body.appendChild(img);
+
+    return new Promise((resolve, reject) =>
+    {
+        const onLoad = () =>
+        {
+            ctx.drawImage(img, svgExtent[0], svgExtent[1], svgExtent[2], svgExtent[3],
+                               0, 0, imageSize[0], imageSize[1]);
+            const dataURI = canvas.toDataURL('image/png');
+            document.body.removeChild(img);
+            resolve(dataURI);
+        }
+
+        const onError = (e) =>
+        {
+          document.body.removeChild(img);
+          reject(`ERROR: ${e}`);
+        }
+
+        img.addEventListener("load", onLoad);
+        img.addEventListener("error", onError);
+        img.src = `data:image/svg+xml;base64,${svgBase64}`;
+    });
+}
+
+
+//==============================================================================
+
+/**
+ * Add transparency to an image.
+ *
+ * @param      {Jimp}   image                A Jimp image
+ * @param      {String}   transparentColour  The transparent colour as a CSS colour value
+ * @return     {boolean}  true if the resulting image is fully transparent
+ */
+function makeTransparent(image, transparentColour)
+{
+    let transparent = undefined;
+    if (transparentColour !== undefined) {
+        transparent = true;
+        const colour = Jimp.cssColorToHex(transparentColour);
+        const imageBitmapData = image.bitmap.data;
+        for (let index = 0; index < imageBitmapData.length; index += 4) {
+            if (colour === imageBitmapData.readUInt32BE(index)) {
+                imageBitmapData.fill(0, index, index+4);
+            } else if (transparent && imageBitmapData[index + 3]) {
+                transparent = false;
+            }
+        }
+    }
+    return transparent;
+}
+
+//==============================================================================
+
+/**
+ * Test if an image is transparent.
+ *
+ * @param      {Jimp}   image   A Jimp image
+ * @return     {boolean}  true if the resulting image is fully transparent
+ */
+function transparent(image)
+{
+    const imageBitmapData = image.bitmap.data;
+    for (let index = 0; index < imageBitmapData.length; index += 4) {
+        if (imageBitmapData[index + 3]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//==============================================================================
+
+class TileMaker
+{
+	constructor(map, outputDirectory)
+	{
+        this._map = map;
+		this._tileDims = [Math.ceil(this._map.size[0]/TILE_PIXEL_SIZE[0]),
+                          Math.ceil(this._map.size[1]/TILE_PIXEL_SIZE[1])];
+        this._tiledSize = [TILE_PIXEL_SIZE[0]*this._tileDims[0],
+                           TILE_PIXEL_SIZE[1]*this._tileDims[1]];
+        const maxTileDim = Math.max(this._tileDims[0], this._tileDims[1]);
+        this._fullZoom = Math.ceil(Math.log2(maxTileDim));
+        this._outputDirectory = outputDirectory;
+		this._tileDirectory = path.join(outputDirectory, 'tiles');
+	}
+
+    async tileZoomLevel_(layer, zoomLevel, svgBuffer, svgExtent, imageSize, page)
+    //===========================================================================
+    {
+        const zoomScale = 2**(this._fullZoom - zoomLevel);
+        const zoomedSize = [imageSize[0]/zoomScale, imageSize[1]/zoomScale];
+
+        const pngDataURI = await page.evaluate(svgToPng, svgBuffer.toString('base64'), svgExtent, zoomedSize);
+
+        const pngImage = await Jimp.create(Buffer.from(pngDataURI.substr('data:image/png;base64,'.length), 'base64'));
+
+        const origin = layer.origin ? [layer.origin[0]/zoomScale, layer.origin[1]/zoomScale]
+                                    : [0, 0];
+
+        const xTileStart = Math.floor(origin[0]/TILE_PIXEL_SIZE[0]);
+        const xStart = xTileStart*TILE_PIXEL_SIZE[0] - origin[0];
+
+        const yTileStart = Math.floor(origin[1]/TILE_PIXEL_SIZE[1]);
+        const yStart = zoomedSize[1] + origin[1] - yTileStart*TILE_PIXEL_SIZE[1] - TILE_PIXEL_SIZE[1];
+
+        // Create tiles and write them out
+
+        const tilePromises = [];
+        for (let x = xTileStart, xOffset = xStart;
+             xOffset < zoomedSize[0];
+             x +=1, xOffset += TILE_PIXEL_SIZE[0]) {
+
+            const tileDirectory = path.join(this._tileDirectory, layer.id, `${zoomLevel}`, `${x}`);
+            let dirExists = fs.existsSync(tileDirectory);
+
+            for (let y = yTileStart, yOffset = yStart;
+                 yOffset > -TILE_PIXEL_SIZE[1];
+                 y +=1, yOffset -= TILE_PIXEL_SIZE[1]) {
+
+                const tile = await cropImage.cropImage(pngImage, xOffset, yOffset,
+                                                       TILE_PIXEL_SIZE[0], TILE_PIXEL_SIZE[1]);
+                if (!makeTransparent(tile, layer.transparent) && !transparent(tile)) {
+                    if (!dirExists) {
+                        fs.mkdirSync(tileDirectory, {recursive: true, mode: 0o755});
+                        dirExists = true;
+                    }
+                    tilePromises.push(tile.writeAsync(path.join(tileDirectory, `${y}.png`)));
+                }
+            }
+        }
+        console.log(`Tiled ${layer.id} at zoom level ${zoomLevel}`)
+
+        return Promise.all(tilePromises);
+    }
+
+    async tileLayer_(layer, browser)
+    //==============================
+    {
+        const svgBuffer = fs.readFileSync(layer.source);
+
+        let svgExtent = layer.sourceExtent;
+        if (!svgExtent) {
+            const dimensions = sizeOf(svgBuffer);
+            svgExtent = [0, 0, dimensions.width, dimensions.height];
+        }
+
+        let imageSize = this._map.size;
+        if (layer.resolution) {
+            imageSize = [layer.resolution*svgExtent[2], layer.resolution*svgExtent[3]];
+        }
+
+        const zoomRange = layer.zoom || [0, this._fullZoom];
+
+        // Only generate tiles if SVG and/or map and/or layer attributes have changed
+
+        let md5Hash = crypto.createHash('md5')
+                           .update(layer.id)
+                           .update(svgBuffer)
+                           .update(JSON.stringify(this._map.size))
+                           .update(JSON.stringify(layer.sourceExtent || []))
+                           .update(JSON.stringify(layer.resolution || 1))
+                           .update(JSON.stringify(layer.transparent || null))
+                           .update(JSON.stringify(zoomRange))
+                           .digest("hex");
+        const md5File = path.join(this._tileDirectory, layer.id, 'layer.md5');
+        if (fs.existsSync(md5File) && fs.readFileSync(md5File, 'utf-8') === md5Hash) {
+            return;
+        }
+
+        const page = await browser.newPage();
+        page.on('console', msg => console.log(`Layer ${layer.id}:`, msg.text()));
+
+        // Tile all zoom levels in the layer
+
+        const zoomPromises = [];
+        for (let z = zoomRange[0]; z <= zoomRange[1]; z += 1) {
+            zoomPromises.push(this.tileZoomLevel_(layer, z, svgBuffer, svgExtent, imageSize, page));
+        }
+        await Promise.all(zoomPromises);
+
+        // Save md5 hash with layer
+
+        fs.writeFileSync(md5File, md5Hash);
+    }
+
+    async makeTiles()
+    //===============
+    {
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-dev-shm-usage']});
+
+        // Tile all layers
+
+        const layerPromises = [];
+
+        for (const layer of this._map.layers) {
+            layerPromises.push(this.tileLayer_(layer, browser));
+        }
+
+        // Wait for layer tiling complete
+
+        await Promise.all(layerPromises);
+
+        // Then close the browser
+
+        await browser.close();
+    }
+}
+
+//==============================================================================
+
+module.exports.TileMaker = TileMaker;
+
+//==============================================================================
